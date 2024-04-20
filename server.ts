27051aa59b0a3c3b653e194c8eb8fd9d25729f4a
ayaniegain <ayaniegain@gmail.com>
2024-04-20 13:07:08 +0530

    config configure

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import { config } from "./config/config";
+import app from "./src/app";
+import colors from "colors";
+
+function serverUp() {
+  const port = config.port || 8080;
+
+  app.listen(port, () => {
+    console.log(colors.yellow(`server is running on port : ${port}`));
+  });
+}
+
+serverUp();
